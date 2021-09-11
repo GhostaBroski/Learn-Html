41a48ea5a4f357ced51e10ad2e1cb17c76ea5628
GhostaBroski <ghostabroski@gmail.com>
2021-09-11 16:08:27 +0100

    hello

---
+++ lesson_6/hello.js
@@ -0,0 +1,31 @@
+console.log("hello world");
+//practise: Comupte array sum
+let sum = 0;
+let nums = [1000000,999,9999]
+for(let a = 0; a< nums.length; a++){
+    sum = nums[a] + sum;
+}
+console.log("sum", sum)
+ 
+function computeSum(nums) {
+    let sum = 0
+    for(let x = 0; x < nums.length; x++) {
+        sum = sum + nums[x];
+    }
+    return sum;
+}
+
+let sum2 = computeSum([1,2,3,4,5]);
+console.log("sum2",sum2)
+
+function computeCheck(a) {
+     
+    if(a% 3 ==0 ){
+        return  true;
+    }
+    if(a% 7 == 0){
+        return true
+    }
+    return false
+}
+console.log("computeCheck", computeCheck(11))
