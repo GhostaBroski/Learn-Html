ed2b77c532e046b6f348b93169e9761e2c071bd2
GhostaBroski <ghostabroski@gmail.com>
2021-09-18 15:38:19 +0100

    l

--- lesson_6/another.js
+++ lesson_6/another.js
@@ -8,3 +8,9 @@ function min(a,b){
     
 }
 console.log("min", min(122,7))
+let angle = Math.PI / 4;
+let x = Math.sin(angle) ** 2 + Math.cos(angle) ** 2
+console.log("sin(X)^2 + cos(x)^2 =", x);
+let y = 3.673848
+
+console.log(Math.round(y)); 
